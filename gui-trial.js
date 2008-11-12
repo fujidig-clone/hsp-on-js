bbc623ea9e9ce5f18707aad3880f5501c84c4a83
fujidig <fujidig@d0d07461-0603-4401-acd4-de1884942a52>
2008-11-12 04:16:03 +0000

    GUI の試みを追加
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/hsp-on-js/trunk@23304 d0d07461-0603-4401-acd4-de1884942a52

---
+++ gui-trial.js
@@ -0,0 +1,60 @@
+function Color(r, g, b, a) {
+	this.r = r & 255;
+	this.g = g & 255;
+	this.b = b & 255;
+	if(a == undefined) {
+		this.a = 1;
+	} else {
+		this.a = Math.min(Math.max(a, 0), 1);
+	}
+}
+
+HSPonJS.Utils.objectExtend(Color, {
+	toRGBAString: function toRGBAString(r, g, b, a) {
+		return 'rgba('+r+','+g+','+b+','+a+')';
+	},
+	toRGBString: function toRGBString(r, g, b) {
+		return 'rgb('+r+','+g+','+b+')';
+	},
+	toHexString: function toHexString(r, g, b) {
+		return '#' + (0x1000000 | r << 16 | g << 8 | b).toString(16).slice(1);
+	}
+});
+
+Color.prototype = {
+	toRGBAString: function toRGBAString() {
+		return Color.toRGBAString(this.r, this.g, this.b, this.a);
+	},
+	toRGBString: function toRGBString() {
+		return Color.toRGBString(this.r, this.g, this.b);
+	},
+	toHexString: function toHexString() {
+		return Color.toHexString(this.r, this.g, this.b);
+	},
+	toString: function toString() {
+		return '<Color: '+this.toRGBAString()+'>';
+	}
+};
+
+
+with(HSPonJS) {
+	BuiltinFuncs[Token.Type.EXTCMD][0x18] = function color(r, g, b) {
+		this.scanArgs(arguments, 'NNN');
+		r = r ? r.toIntValue()._value : 0;
+		g = g ? g.toIntValue()._value : 0;
+		b = b ? b.toIntValue()._value : 0;
+		this.ctx.fillStyle = Color.toRGBString(r, g, b);
+	};
+	BuiltinFuncs[Token.Type.EXTCMD][0x31] = function boxf(x1, y1, x2, y2) {
+		this.scanArgs(arguments, 'NNNN');
+		x1 = x1 ? x1.toIntValue()._value : 0;
+		y1 = y1 ? y1.toIntValue()._value : 0;
+		x2 = x2 ? x2.toIntValue()._value : this.ctx.canvas.width;
+		y2 = y2 ? y2.toIntValue()._value : this.ctx.canvas.height;
+		var width = Math.abs(x2 - x1) + 1;
+		var height = Math.abs(y2 - y1) + 1;
+		var x = Math.min(x1, x2);
+		var y = Math.min(y1, y2);
+		this.ctx.fillRect(x, y, width, height);
+	};
+}
