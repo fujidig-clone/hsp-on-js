c418e1398631407ad04d67959f6674ed76e0de2c
fujidig <fujidig@d0d07461-0603-4401-acd4-de1884942a52>
2008-11-15 21:03:24 +0000

    screen, buffer, gsel を実装
    git-svn-id: http://svn.coderepos.org/share/lang/javascript/hsp-on-js/trunk@23816 d0d07461-0603-4401-acd4-de1884942a52

--- gui-trial.js
+++ gui-trial.js
@@ -54,31 +54,156 @@ var removeEvent = (function(){
         :  function(e, n, f){ delete window['on' + n] };
 })();
 
-HSPonJS.Evaluator.prototype.guiInitialize = function guiInitialize() {
+function Screen() {
+	this.ctx = null;
 	this.currentX = this.currentY = 0;
 	this.currentR = this.currentG = this.currentB = 0;
 	this.mesX = this.mesY = 0;
 	this.mouseX = this.mouseY = this.mouseW = 0;
 	this.fontSize = 18;
 	this.fontStyle = 0;
+	this.width = this.height = null;
+}
 
-	var ctx = this.ctx;
-	ctx.fillStyle = '#fff';
-	ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-	ctx.fillStyle = '#000';
-	ctx.strokeStyle = '#000';
-	ctx.font = this.fontSize+'px monospace';
+Screen.prototype = {
+	setContext: function setContext(ctx) {
+		this.ctx = ctx;
+		this.width = ctx.canvas.width;
+		this.height = ctx.canvas.height;
+	},
+	clear: function clear() {
+		var ctx = this.ctx;
+		ctx.fillStyle = '#fff';
+		ctx.fillRect(0, 0, this.width, this.height);
+		this.selectColor(0, 0, 0);
+		this.setFont('monospace', 18, 0);
+	},
+	selectColor: function selectColor(r, g, b) {
+		this.currentR = r & 255;
+		this.currentG = g & 255;
+		this.currentB = b & 255;
+		this.ctx.fillStyle = this.ctx.strokeStyle = Color.toRGBString(this.currentR, this.currentG, this.currentB);
+	},
+	selectColorByHSV: function selectColorByHSV(h, s, v) {
+		h = h % 192;
+		s = s & 255;
+		v = v & 255;
+		var mv = 255 * 32;
+		var mp = 255 * 16;
+		var i = h / 32 | 0; 
+		var t = h % 32;
+		var v1 = (v*(mv-s*32)    +mp)/mv|0;
+		var v2 = (v*(mv-s*t)     +mp)/mv|0;
+		var v3 = (v*(mv-s*(32-t))+mp)/mv|0;
+		var r = 0, g = 0, b = 0;
+		switch(i){
+		case 0:
+		case 6:
+			r=v;	g=v3;	b=v1;	break;
+		case 1:
+			r=v2;	g=v;	b=v1;	break;
+		case 2:
+			r=v1;	g=v;	b=v3;	break;
+		case 3:
+			r=v1;	g=v2;	b=v;	break;
+		case 4:
+			r=v3;	g=v1;	b=v;	break;
+		case 5:
+			r=v;	g=v1;	b=v2;	break;
+		}
+		this.selectColor(r, g, b);
+	},
+	drawEllipse: function drawEllipse(x1, y1, x2, y2, fill_p) {
+		// FIXME アンチエイリアスのかかった線が描画されてしまう
+		var w = (x2 - x1) / 2;
+		var h = (y2 - y1) / 2;
+		var C = 0.5522847498307933;
+		var c_x = C * w;
+		var c_y = C * h;
+		var ctx = this.ctx;
+		ctx.beginPath();
+		ctx.moveTo(x2, y1 + h);
+		ctx.bezierCurveTo(x2, y1 + h - c_y, x1 + w + c_x, y1, x1 + w, y1);
+		ctx.bezierCurveTo(x1 + w - c_x, y1, x1, y1 + h - c_y, x1, y1 + h);
+		ctx.bezierCurveTo(x1, y1 + h + c_y, x1 + w - c_x, y2, x1 + w, y2);
+		ctx.bezierCurveTo(x1 + w + c_x, y2, x2, y1 + h + c_y, x2, y1 + h);
+		if (fill_p) {
+			ctx.fill();
+		} else {
+			ctx.stroke();
+		}
+	},
+	drawText: function drawText(text) {
+		var lines = text.split(/\r\n|[\r\n]/);
+		var ctx = this.ctx;
+		ctx.textBaseline = 'top';
+		this.mesY = this.fontSize;
+		for(var i = 0; i < lines.length; i ++) {
+			ctx.fillText(lines[i], this.currentX, this.currentY);
+			this.mesX = ctx.measureText(lines[i]).width;
+			if(this.fontStyle & 4) {
+				ctx.fillRect(this.currentX, this.currentY + this.mesY - 1, this.mesX, 1);
+			}
+			if(this.fontStyle & 8) {
+				ctx.fillRect(this.currentX, this.currentY + this.mesY / 2, this.mesX, 1);
+			}
+			this.currentY += this.fontSize;
+		}
+	},
+	setFont: function setFont(name, size, style) {
+		var str = "";
+		if(style & 2) {
+			str += "italic ";
+		}
+		if(style & 1) {
+			str += "bold ";
+		}
+		str += size + "px ";
+		str += '"' + name.replace(/[\\"]/g, function(s) { return "\\" + s; }) + '"';
+		this.ctx.font = str;
+		this.fontSize = size;
+		this.fontStyle = style;
+	},
+	drawLine: function drawLine(x1, y1, x2, y2) {
+		// FIXME アンチエイリアスのかかった線が描画されてしまう
+		this.currentX = x1;
+		this.currentY = y1;
+		var ctx = this.ctx;
+		ctx.beginPath();
+		ctx.moveTo(x2 + 0.5, y2 + 0.5);
+		ctx.lineTo(x1 + 0.5, y1 + 0.5);
+		ctx.stroke();
+	},
+	drawRect: function drawRect(x1, y1, x2, y2) {
+		var width = Math.abs(x2 - x1) + 1;
+		var height = Math.abs(y2 - y1) + 1;
+		var x = Math.min(x1, x2);
+		var y = Math.min(y1, y2);
+		this.ctx.fillRect(x, y, width, height);
+	}
+};
+
+HSPonJS.Evaluator.prototype.guiInitialize = function guiInitialize(iframe, width, height) {
+	this.iframe = iframe;
+	var doc = this.iframeDoc = iframe.contentWindow.document;
+	var mainScreen = this.mainScreen = new Screen;
 	
+	this.changeMainCanvas(width, height);
 	
-	ctx.canvas.style.position = 'relative'; // for layerX, layerY in Firefox
+	this.currentScreen = mainScreen;
+	this.currentScreenId = 0;
+	this.screens = [mainScreen];
+	
+	var canvas = mainScreen.ctx.canvas;
+	canvas.style.position = 'relative'; // for layerX, layerY in Firefox
 	var self = this;
 	function onmousemove(e) {
 		if(e.offsetX != undefined) {
-			self.mouseX = e.offsetX;
-			self.mouseY = e.offsetY;
+			mainScreen.mouseX = e.offsetX;
+			mainScreen.mouseY = e.offsetY;
 		} else {
-			self.mouseX = e.layerX;
-			self.mouseY = e.layerY;
+			mainScreen.mouseX = e.layerX;
+			mainScreen.mouseY = e.layerY;
 		}
 	}
 	this.keyPressed = [];
@@ -113,36 +238,53 @@ HSPonJS.Evaluator.prototype.guiInitialize = function guiInitialize() {
 	}
 	function onscroll(e) {
 		if(e.wheelDelta) {
-			self.mouseW = e.wheelDelta;
+			mainScreen.mouseW = e.wheelDelta;
 		} else {
-			self.mouseW = e.detail * -40;
+			mainScreen.mouseW = e.detail * -40;
 		}
 		e.preventDefault();
 	}
 	
-	var doc = this.iframe.contentWindow.document;
-	addEvent(ctx.canvas, 'mousemove', onmousemove);
+	addEvent(canvas, 'mousemove', onmousemove);
 	addEvent(doc, 'keydown', onkeydown);
 	addEvent(doc, 'keypress', onkeypress);
 	addEvent(doc, 'keyup', onkeyup);
 	addEvent(doc, 'contextmenu', oncontextmenu);
 	addEvent(doc, 'DOMMouseScroll', onscroll);
 	addEvent(doc, 'mousewheel', onscroll);
-	addEvent(ctx.canvas, 'mousedown', onmousedown);
-	addEvent(ctx.canvas, 'mouseup', onmouseup);
+	addEvent(canvas, 'mousedown', onmousedown);
+	addEvent(canvas, 'mouseup', onmouseup);
 	
 	this.removeEvents = function() {
-		removeEvent(ctx.canvas, 'mousemove', onmousemove);
+		removeEvent(canvas, 'mousemove', onmousemove);
 		removeEvent(doc, 'keypress', onkeypress);
 		removeEvent(doc, 'keydown', onkeydown);
 		removeEvent(doc, 'keyup', onkeyup);
 		removeEvent(doc, 'contextmenu', oncontextmenu);
 		removeEvent(doc, 'DOMMouseScroll', onscroll);
 		removeEvent(doc, 'mousewheel', onscroll);
-		removeEvent(ctx.canvas, 'mousedown', onmousedown);
-		removeEvent(ctx.canvas, 'mouseup', onmouseup);
+		removeEvent(canvas, 'mousedown', onmousedown);
+		removeEvent(canvas, 'mouseup', onmouseup);
 	};
 };
+
+HSPonJS.Evaluator.prototype.removeCanvasElement = function removeCanvasElement() {
+	if(this.mainScreen.ctx) {
+		this.iframeDoc.body.removeChild(this.mainScreen.ctx.canvas);
+	}
+};
+
+HSPonJS.Evaluator.prototype.changeMainCanvas = function changeMainCanvas(width, height) {
+	this.iframe.setAttribute('width', width);
+	this.iframe.setAttribute('height', height);
+	var canvas = this.iframeDoc.createElement('canvas');
+	canvas.width = width;
+	canvas.height = height;
+	this.removeCanvasElement();
+	this.iframeDoc.body.appendChild(canvas);
+	this.mainScreen.setContext(canvas.getContext('2d'));
+	this.mainScreen.clear();
+}
 
 with(HSPonJS) {
 	Utils.objectExtend(BuiltinFuncs[Token.Type.EXTCMD], {
@@ -161,15 +303,15 @@ with(HSPonJS) {
 		},
 		0x0c: function pset(x, y) {
 			this.scanArgs(arguments, 'NN');
-			x = x ? x.toIntValue()._value : this.currentX;
-			y = y ? y.toIntValue()._value : this.currentY;
-			this.ctx.fillRect(x, y, 1, 1);
+			x = x ? x.toIntValue()._value : this.currentScreen.currentX;
+			y = y ? y.toIntValue()._value : this.currentScreen.currentY;
+			this.currentScreen.ctx.fillRect(x, y, 1, 1);
 		},
 		0x0d: function pget(x, y) {
 			this.scanArgs(arguments, 'NN');
-			x = x ? x.toIntValue()._value : this.currentX;
-			y = y ? y.toIntValue()._value : this.currentY;
-			var ctx = this.ctx;
+			x = x ? x.toIntValue()._value : this.currentScreen.currentX;
+			y = y ? y.toIntValue()._value : this.currentScreen.currentY;
+			var ctx = this.currentScreen.ctx;
 			var r, g, b;
 			if(x < 0 || x >= ctx.canvas.width || y < 0 || y >= ctx.canvas.height) {
 				r = g = b = 255;
@@ -177,154 +319,107 @@ with(HSPonJS) {
 				var data = ctx.getImageData(x, y, 1, 1).data;
 				r = data[0], g = data[1], b = data[2];
 			}
-			ctx.fillStyle = ctx.strokeStyle = Color.toRGBString(r, g, b);
-			this.currentR = r;
-			this.currentG = g;
-			this.currentB = b;
+			this.currentScreen.selectColor(r, g, b);
 		},
 		0x0f: function mes(text) {
 			this.scanArgs(arguments, '.?');
 			text = text ? CP932.decode(text.toStrValue()._value) : "";
-			lines = text.split(/\r\n|[\r\n]/);
-			var ctx = this.ctx;
-			ctx.textBaseline = 'top';
-			this.mesY = this.fontSize;
-			for(var i = 0; i < lines.length; i ++) {
-				ctx.fillText(lines[i], this.currentX, this.currentY);
-				this.mesX = ctx.measureText(lines[i]).width;
-				if(this.fontStyle & 4) {
-					ctx.fillRect(this.currentX, this.currentY + this.mesY - 1, this.mesX, 1);
-				}
-				if(this.fontStyle & 8) {
-					ctx.fillRect(this.currentX, this.currentY + this.mesY / 2, this.mesX, 1);
-				}
-				this.currentY += this.fontSize;
-			}
+			this.currentScreen.drawText(text);
 		},
 		0x11: function pos(x, y) {
 			this.scanArgs(arguments, 'NN');
-			if(x) this.currentX = x.toIntValue()._value;
-			if(y) this.currentY = y.toIntValue()._value;
+			if(x) this.currentScreen.currentX = x.toIntValue()._value;
+			if(y) this.currentScreen.currentY = y.toIntValue()._value;
 		},
 		0x12: function circle(x1, y1, x2, y2, fill_p) {
-			// FIXME アンチエイリアスのかかった線が描画されてしまう
 			this.scanArgs(arguments, 'NNNNN');
 			x1 = x1 ? x1.toIntValue()._value : 0;
 			y1 = y1 ? y1.toIntValue()._value : 0;
-			x2 = x2 ? x2.toIntValue()._value : this.ctx.canvas.width;
-			y2 = y2 ? y2.toIntValue()._value : this.ctx.canvas.height;
+			x2 = x2 ? x2.toIntValue()._value : this.currentScreen.width;
+			y2 = y2 ? y2.toIntValue()._value : this.currentScreen..height;
 			fill_p = fill_p ? fill_p.toIntValue()._value : 1;
 			
-			var w = (x2 - x1) / 2;
-			var h = (y2 - y1) / 2;
-			var C = 0.5522847498307933;
-			var c_x = C * w;
-			var c_y = C * h;
-			var ctx = this.ctx;
-			ctx.beginPath();
-			ctx.moveTo(x2, y1 + h);
-			ctx.bezierCurveTo(x2, y1 + h - c_y, x1 + w + c_x, y1, x1 + w, y1);
-			ctx.bezierCurveTo(x1 + w - c_x, y1, x1, y1 + h - c_y, x1, y1 + h);
-			ctx.bezierCurveTo(x1, y1 + h + c_y, x1 + w - c_x, y2, x1 + w, y2);
-			ctx.bezierCurveTo(x1 + w + c_x, y2, x2, y1 + h + c_y, x2, y1 + h);
-			if (fill_p) {
-				ctx.fill();
-			} else {
-				ctx.stroke();
-			}
+			this.currentScreen.drawEllipse(x1, y1, x2, y2, fill_p);
 		},
 		0x14: function font(name, size, style) {
 			this.scanArgs(arguments, 'sNN');
 			name = CP932.decode(name.toStrValue()._value);
 			size = size ? size.toIntValue()._value : 18;
 			style = style ? style.toIntValue()._value : 0;
-			var str = "";
-			if(style & 2) {
-				str += "italic ";
-			}
-			if(style & 1) {
-				str += "bold ";
-			}
-			str += size + "px ";
-			str += '"' + name.replace(/[\\"]/g, function(s) { return "\\" + s}) + '"';
-			this.ctx.font = str;
-			this.fontSize = size;
-			this.fontStyle = style;
+			this.currentScreen.setFont(name, size, style);
 		},
 		0x18: function color(r, g, b) {
 			this.scanArgs(arguments, 'NNN');
 			r = r ? r.toIntValue()._value & 255 : 0;
 			g = g ? g.toIntValue()._value & 255 : 0;
 			b = b ? b.toIntValue()._value & 255 : 0;
-			this.ctx.fillStyle = this.ctx.strokeStyle = Color.toRGBString(r, g, b);
-			this.currentR = r;
-			this.currentG = g;
-			this.currentB = b;
+			this.currentScreen.selectColor(r, g, b);
+		},
+		0x1d: function gsel(screenId) {
+			this.scanArgs(arguments, 'N');
+			screenId = screenId ? screenId.toIntValue()._value : 0;
+			var screen = this.screens[screenId];
+			if(!screen) throw new HSPError(ErrorCode.ILLEGAL_FUNCTION);
+			this.currentScreen = screen;
+			this.currentScreenId = screenId;
 		},
 		0x22: function hsvcolor(h, s, v) {
 			this.scanArgs(arguments, 'NNN');
 			h = h ? h.toIntValue()._value % 192 : 0;
 			s = s ? s.toIntValue()._value & 255 : 0;
 			v = v ? v.toIntValue()._value & 255 : 0;
-			var mv = 255 * 32;
-			var mp = 255 * 16;
-			var i = h / 32 | 0; 
-			var t = h % 32;
-			var v1 = (v*(mv-s*32)    +mp)/mv|0;
-			var v2 = (v*(mv-s*t)     +mp)/mv|0;
-			var v3 = (v*(mv-s*(32-t))+mp)/mv|0;
-			var r = 0, g = 0, b = 0;
-			switch(i){
-			case 0:
-			case 6:
-				r=v;	g=v3;	b=v1;	break;
-			case 1:
-				r=v2;	g=v;	b=v1;	break;
-			case 2:
-				r=v1;	g=v;	b=v3;	break;
-			case 3:
-				r=v1;	g=v2;	b=v;	break;
-			case 4:
-				r=v3;	g=v1;	b=v;	break;
-			case 5:
-				r=v;	g=v1;	b=v2;	break;
-			}
-			this.ctx.fillStyle = this.ctx.strokeStyle = Color.toRGBString(r, g, b);
-			this.currentR = r;
-			this.currentG = g;
-			this.currentB = b;
+			this.currentScreen.selectColorByHSV(h, s, v);
 		},
 		0x23: function getkey(v, key) {
 			this.scanArgs(arguments, 'vN');
 			key = key ? key.toIntValue()._value : 1;
 			v.assign(new IntValue(this.keyPressed[key]));
 		},
+		0x29: function buffer(screenId, width, height) {
+			this.scanArgs(arguments, 'NNNNNNNN');
+			screenId = screenId ? screenId.toIntValue()._value : 0;
+			width = width ? width.toIntValue()._value : 640;
+			height = height ? height.toIntValue()._value : 480;
+			var screen;
+			if(!(screen = this.screens[screenId])) {
+				screen = this.screens[screenId] = new Screen;
+			}
+			var canvas = this.iframeDoc.createElement('canvas');
+			canvas.width = width;
+			canvas.height = height;
+			screen.setContext(canvas.getContext('2d'));
+			screen.clear();
+			this.currentScreen = screen;
+			this.currentScreenId = screenId;
+		},
+		0x2a: function screen(screenId, width, height) {
+			this.scanArgs(arguments, 'NNNNNNNN');
+			screenId = screenId ? screenId.toIntValue()._value : 0;
+			width = width ? width.toIntValue()._value : 640;
+			height = height ? height.toIntValue()._value : 480;
+			if(screenId == 0) {
+				this.changeMainCanvas(width, height);
+				this.currentScreen = this.mainScreen;
+				this.currentScreenId = 0;
+			} else {
+				throw new HSPError(ErrorCode.ILLEGAL_FUNCTION, 'ID 0 以外の screen は未対応');
+			}
+		},
 		0x2f: function line(x1, y1, x2, y2) {
-			// FIXME アンチエイリアスのかかった線が描画されてしまう
 			this.scanArgs(arguments, 'NNNN');
 			x1 = x1 ? x1.toIntValue()._value : 0;
 			y1 = y1 ? y1.toIntValue()._value : 0;
-			x2 = x2 ? x2.toIntValue()._value : this.currentX;
-			y2 = y2 ? y2.toIntValue()._value : this.currentY;
-			this.currentX = x1;
-			this.currentY = y1;
-			var ctx = this.ctx;
-			ctx.beginPath();
-			ctx.moveTo(x2 + 0.5, y2 + 0.5);
-			ctx.lineTo(x1 + 0.5, y1 + 0.5);
-			ctx.stroke();
+			x2 = x2 ? x2.toIntValue()._value : this.currentScreen.currentX;
+			y2 = y2 ? y2.toIntValue()._value : this.currentScreen.currentY;
+			this.currentScreen.drawLine(x1, y1, x2, y2);
 		},
 		0x31: function boxf(x1, y1, x2, y2) {
 			this.scanArgs(arguments, 'NNNN');
 			x1 = x1 ? x1.toIntValue()._value : 0;
 			y1 = y1 ? y1.toIntValue()._value : 0;
-			x2 = x2 ? x2.toIntValue()._value : this.ctx.canvas.width;
-			y2 = y2 ? y2.toIntValue()._value : this.ctx.canvas.height;
-			var width = Math.abs(x2 - x1) + 1;
-			var height = Math.abs(y2 - y1) + 1;
-			var x = Math.min(x1, x2);
-			var y = Math.min(y1, y2);
-			this.ctx.fillRect(x, y, width, height);
+			x2 = x2 ? x2.toIntValue()._value : this.currentScreen.width;
+			y2 = y2 ? y2.toIntValue()._value : this.currentScreen.height;
+			this.currentScreen.drawRect(x1, y1, x2, y2);
 		},
 		0x34: function stick(v, notrigerMask) {
 			this.scanArgs(arguments, 'vNN');
@@ -349,14 +444,14 @@ with(HSPonJS) {
 	});
 	Utils.objectExtend(BuiltinFuncs[Token.Type.EXTSYSVAR], {
 		0x000: function mousex() {
-			return new IntValue(this.mouseX);
+			return new IntValue(this.currentScreen.mouseX);
 		},
 		0x001: function mousey() {
-			return new IntValue(this.mouseY);
+			return new IntValue(this.currentScreen.mouseY);
 		},
 		0x002: function mousew() {
-			var result = this.mouseW;
-			this.mouseW = 0;
+			var result = this.currentScreen.mouseW;
+			this.currentScreen.mouseW = 0;
 			return new IntValue(result);
 		},
 		0x100: function ginfo(type) {
@@ -364,27 +459,27 @@ with(HSPonJS) {
 			type = type.toIntValue()._value;
 			switch(type) {
 			case 12: // ginfo_winx
-				return new IntValue(this.ctx.canvas.width);
+				return new IntValue(this.currentScreen.width);
 			case 13: // ginfo_winy
-				return new IntValue(this.ctx.canvas.height);
+				return new IntValue(this.currentScreen.height);
 			case 14: // ginfo_mesx
-				return new IntValue(this.mesX);
+				return new IntValue(this.currentScreen.mesX);
 			case 15: // ginfo_mesy
-				return new IntValue(this.mesY);
+				return new IntValue(this.currentScreen.mesY);
 			case 16: // ginfo_r
-				return new IntValue(this.currentR);
+				return new IntValue(this.currentScreen.currentR);
 			case 17: // ginfo_g
-				return new IntValue(this.currentG);
+				return new IntValue(this.currentScreen.currentG);
 			case 18: // ginfo_b
-				return new IntValue(this.currentB);
+				return new IntValue(this.currentScreen.currentB);
 			case 22: // ginfo_cx
-				return new IntValue(this.currentX);
+				return new IntValue(this.currentScreen.currentX);
 			case 23: // ginfo_cy
-				return new IntValue(this.currentY);
+				return new IntValue(this.currentScreen.currentY);
 			case 26: // ginfo_sx
-				return new IntValue(this.ctx.canvas.width);
+				return new IntValue(this.currentScreen.width);
 			case 27: // ginfo_sy
-				return new IntValue(this.ctx.canvas.height);
+				return new IntValue(this.currentScreen.height);
 			default:
 				throw new HSPError(ErrorCode.ILLEGAL_FUNCTION);
 			}
